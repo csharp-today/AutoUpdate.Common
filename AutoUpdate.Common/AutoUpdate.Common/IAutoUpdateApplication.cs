1e8a6dd1ac96edda039101cd2412aa65f5234b87
Mariusz Bojkowski <mariusz.bojkowski@gmail.com>
2015-12-27 06:34:20 +0100

    Public IAutoUpdateApplication interface

---
+++ AutoUpdate.Common/AutoUpdate.Common/IAutoUpdateApplication.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoUpdate.Common
+{
+    public interface IAutoUpdateApplication
+    {
+        void Start(string[] args);
+    }
+}
