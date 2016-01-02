7b9c675d40224286175145c8da217836352c85b0
Mariusz Bojkowski <mariusz.bojkowski@gmail.com>
2016-01-03 00:46:58 +0100

    Add simple auto update interface for services

---
+++ AutoUpdate.Common/AutoUpdate.Common/IAutoUpdateService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoUpdate.Common
+{
+    public interface IAutoUpdateService
+    {
+        void Start();
+        void Stop();
+    }
+}
